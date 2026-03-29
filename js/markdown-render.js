8db7188c896ca5a9483391ca5ae6dde01055c889
ExistensialReset <elinor.frejd@proton.me>
2026-03-29 23:37:40 +0200

    Update markdown-render.js

--- js/markdown-render.js
+++ js/markdown-render.js
@@ -1,3 +1,20 @@
+const folderOrder = [
+  "00_core",
+  "01_principles",
+  "02_documents",
+  "03_governance",
+  "04_simulations",
+  "05_facilitators",
+  "06_justice",
+  "07_technology",
+  "08_environmental",
+  "09_economics",
+  "10_svenska"
+];
+
+const sidebar = document.getElementById("sidebar");
+const content = document.getElementById("content");
+
 fetch('./pages.json')
   .then(res => res.json())
   .then(pages => {
@@ -53,3 +70,14 @@ fetch('./pages.json')
         if (firstFolderFiles.length > 0) loadPage(firstFolderFiles[0].path);
       });
   });
+
+function loadPage(path) {
+  fetch(path)
+    .then(res => res.text())
+    .then(md => {
+      content.innerHTML = marked.parse(md);
+    })
+    .catch(() => {
+      content.innerHTML = "❌ Could not load file: " + path;
+    });
+}
